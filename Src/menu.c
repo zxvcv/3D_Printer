ff4ee5134af15c19b39552c501e8fdea21e6b1e5
zxvcv <ppiskorz0@gmail.com>
2019-11-08 19:45:13 +0100

    v0.1.3

--- Src/menu.c
+++ Src/menu.c
@@ -26,6 +26,9 @@ typedef struct PrinterSettings{
 	double maxPosY;
 	double maxPosZ;
 	double minStep;
+
+	double position[3];
+	double speed[3];
 }PrinterSettings;
 
 PROGRAM_STATE actualPrgState;
@@ -34,6 +37,7 @@ uint8_t markVerticalPos;
 PrinterSettings printerSettings;
 double pos[3] = {100.00, 100.00, 100.0};
 double mov[3] = {0.0, 0.0, 0.0};
+int spd[3] = {88888, 77777, 11111};
 
 char data[20]; //for sprintf
 
@@ -47,7 +51,7 @@ void draw_arrow(uint8_t x, uint8_t y, uint8_t color);
 void draw_coordinate_diagram(uint8_t x, uint8_t y, uint8_t color);
 void draw_edit_mark(bool on);
 double updateDoubleValue(uint8_t xDraw, uint8_t yDraw, uint8_t charWidth, bool sign, char* format, double oldValue);
-
+int updateIntegerValue(uint8_t xDraw, uint8_t yDraw, uint8_t charWidth, bool sign, char* format, int oldValue);
 
 
 /* FUNCTIONS DEFINITIONS */
@@ -115,64 +119,126 @@ void menu_run(KeyboardButtons buttState){
 		case ST3_MOVE_HEAD:
 			switch (buttState) {
 				case BUTT_2:
-					if(arrowLinePos > 1 && markVerticalPos == 0){
-						draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos + 2, 0);
+					if(arrowLinePos > 3 && markVerticalPos == 0){
+						draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos, 0);
 						--arrowLinePos;
-						draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos + 2, 255);
+						draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
 						ST7565_display();
 					}
 					break;
 				case BUTT_8:
 					if(arrowLinePos < 6 && markVerticalPos == 0){
-						draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos + 2, 0);
+						draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos, 0);
 						++arrowLinePos;
-						draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos + 2, 255);
+						draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
 						ST7565_display();
 					}
 					break;
 				case BUTT_6:
-					if(markVerticalPos < 2 && arrowLinePos >= 2 && arrowLinePos <= 4){
+					if(markVerticalPos < 3 && arrowLinePos >= 3 && arrowLinePos <= 6){
 						++markVerticalPos;
-						int y = LINE_HIGH * (arrowLinePos + 1) + arrowLinePos;
-						if(markVerticalPos == 1){
-							ST7565_drawline(14, y, 48, y, 255);
-							ST7565_display();
+						if(arrowLinePos == 3){
+							if(markVerticalPos == 1){
+								draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos, 0);
+								draw_arrow(14, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
+								ST7565_display();
+							}
+							if(markVerticalPos == 2){
+								draw_arrow(14, arrowLinePos * LINE_HIGH + arrowLinePos, 0);
+								draw_arrow(52, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
+								ST7565_display();
+							}
+							if(markVerticalPos == 3){
+								draw_arrow(52, arrowLinePos * LINE_HIGH + arrowLinePos, 0);
+								draw_arrow(96, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
+								ST7565_display();
+							}
+
 						}
-						if(markVerticalPos == 2){
-							ST7565_drawline(14, y, 48, y, 0);
-							ST7565_drawline(52, y, 92, y, 255);
-							ST7565_display();
+						else{
+							int y = arrowLinePos * LINE_HIGH + arrowLinePos + 6;
+							if(markVerticalPos == 1){
+								draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos, 0);
+								ST7565_drawline(14, y, 48, y, 255);
+								ST7565_display();
+							}
+							if(markVerticalPos == 2){
+								ST7565_drawline(14, y, 48, y, 0);
+								ST7565_drawline(52, y, 92, y, 255);
+								ST7565_display();
+							}
+							if(markVerticalPos == 3){
+								ST7565_drawline(52, y, 92, y, 0);
+								ST7565_drawline(96, y, 124, y, 255);
+								ST7565_display();
+							}
 						}
+
 					}
 					break;
 				case BUTT_4:
 					if(markVerticalPos > 0){
 						--markVerticalPos;
-						int y = LINE_HIGH * (arrowLinePos + 1) + arrowLinePos;
-						if(markVerticalPos == 0){
-							ST7565_drawline(14, y, 48, y, 0);
-							ST7565_display();
+
+						if(arrowLinePos == 3){
+							if(markVerticalPos == 0){
+								draw_arrow(14, arrowLinePos * LINE_HIGH + arrowLinePos, 0);
+								draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
+								ST7565_display();
+							}
+							if(markVerticalPos == 1){
+								draw_arrow(52, arrowLinePos * LINE_HIGH + arrowLinePos, 0);
+								draw_arrow(14, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
+								ST7565_display();
+							}
+							if(markVerticalPos == 2){
+								draw_arrow(96, arrowLinePos * LINE_HIGH + arrowLinePos, 0);
+								draw_arrow(52, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
+								ST7565_display();
+							}
+
 						}
-						if(markVerticalPos == 1){
-							ST7565_drawline(52, y, 92, y, 0);
-							ST7565_drawline(14, y, 48, y, 255);
-							ST7565_display();
+						else{
+							int y = arrowLinePos * LINE_HIGH + arrowLinePos + 6;
+							if(markVerticalPos == 0){
+								draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
+								ST7565_drawline(14, y, 48, y, 0);
+								ST7565_display();
+							}
+							if(markVerticalPos == 1){
+								ST7565_drawline(52, y, 92, y, 0);
+								ST7565_drawline(14, y, 48, y, 255);
+								ST7565_display();
+							}
+							if(markVerticalPos == 2){
+								ST7565_drawline(96, y, 124, y, 0);
+								ST7565_drawline(52, y, 92, y, 255);
+								ST7565_display();
+							}
 						}
 					}
-					break;
+					break;//poprawione do tad
 				case BUTT_5:
-					if(markVerticalPos == 1){
-						ST7565_drawline(14, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH, 14+6*6-2, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH, 0);
-						ST7565_display();
-						pos[arrowLinePos - 2] = updateDoubleValue(14, LINE_HIGH * arrowLinePos + arrowLinePos + 1, 6, false, "%6.2f%c", pos[arrowLinePos - 2]);
-						ST7565_drawline(14, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH, 14+6*6-2, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH, 255);
-						ST7565_display();
-					}else if(markVerticalPos == 2){
-						ST7565_drawline(52, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH, 52+7*6-2, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH, 0);
-						ST7565_display();
-						mov[arrowLinePos - 2] = updateDoubleValue(52, LINE_HIGH * arrowLinePos + arrowLinePos + 1, 7, true, "%+7.2f%c", mov[arrowLinePos - 2]);
-						ST7565_drawline(52, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH, 52+7*6-2, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH, 255);
-						ST7565_display();
+					if(arrowLinePos != 3){
+						if(markVerticalPos == 1){
+							ST7565_drawline(14, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 14+6*6-2, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 0);
+							ST7565_display();
+							pos[arrowLinePos - 2] = updateDoubleValue(14, LINE_HIGH * arrowLinePos + arrowLinePos -1, 6, false, "%6.2f%c", pos[arrowLinePos - 4]);
+							ST7565_drawline(14, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 14+6*6-2, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 255);
+							ST7565_display();
+						}else if(markVerticalPos == 2){
+							ST7565_drawline(52, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 52+7*6-2, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 0);
+							ST7565_display();
+							mov[arrowLinePos - 2] = updateDoubleValue(52, LINE_HIGH * arrowLinePos + arrowLinePos - 1, 7, true, "%+7.2f%c", mov[arrowLinePos - 4]);
+							ST7565_drawline(52, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 52+7*6-2, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 255);
+							ST7565_display();
+						}else if(markVerticalPos == 3){
+							ST7565_drawline(96, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 125, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 0);
+							ST7565_display();
+							mov[arrowLinePos - 2] = updateIntegerValue(96, LINE_HIGH * arrowLinePos + arrowLinePos - 1, 5, false, "%5d%c", spd[arrowLinePos - 4]);
+							ST7565_drawline(96, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 125, LINE_HIGH * arrowLinePos + arrowLinePos + LINE_HIGH - 2, 255);
+							ST7565_display();
+						}
 					}
 					break;
 				case BUTT_B:
@@ -251,38 +317,44 @@ void draw_st2_run_program(){
 
 void draw_st3_move_head(){
 	ST7565_drawstring_line(10, 0, "MOVE HEAD");
-	ST7565_drawstring_line(15, 1, "pos");
-	ST7565_drawstring_line(57, 1, "mov");
-	ST7565_drawchar_pixel(6, LINE_HIGH * 2 + 3, 'X');
-	ST7565_drawchar_pixel(6, LINE_HIGH * 3 + 4, 'Y');
-	ST7565_drawchar_pixel(6, LINE_HIGH * 4 + 5, 'Z');
+	ST7565_drawstring_line(20, 3, "pos");
+	ST7565_drawstring_line(58, 3, "mov");
+	ST7565_drawstring_line(102, 3, "spd");
+	ST7565_drawchar_pixel(6, LINE_HIGH * 4 + 3, 'X');
+	ST7565_drawchar_pixel(6, LINE_HIGH * 5 + 4, 'Y');
+	ST7565_drawchar_pixel(6, LINE_HIGH * 6 + 5, 'Z');
 
 	sprintf(data, "%6.2f%c", pos[0], '\0');
-	ST7565_drawstring_pixel(14, LINE_HIGH * 2 + 3, data);
+	ST7565_drawstring_pixel(14, LINE_HIGH * 4 + 3, data);
 	sprintf(data, "%6.2f%c", pos[1], '\0');
-	ST7565_drawstring_pixel(14, LINE_HIGH * 3 + 4, data);
+	ST7565_drawstring_pixel(14, LINE_HIGH * 5 + 4, data);
 	sprintf(data, "%6.2f%c", pos[2], '\0');
-	ST7565_drawstring_pixel(14, LINE_HIGH * 4 + 5, data);
+	ST7565_drawstring_pixel(14, LINE_HIGH * 6 + 5, data);
 	sprintf(data, "%+7.2f%c", mov[0], '\0');
-	ST7565_drawstring_pixel(52, LINE_HIGH * 2 + 3, data);
+	ST7565_drawstring_pixel(52, LINE_HIGH * 4 + 3, data);
 	sprintf(data, "%+7.2f%c", mov[1], '\0');
-	ST7565_drawstring_pixel(52, LINE_HIGH * 3 + 4, data);
+	ST7565_drawstring_pixel(52, LINE_HIGH * 5 + 4, data);
 	sprintf(data, "%+7.2f%c", mov[2], '\0');
-	ST7565_drawstring_pixel(52, LINE_HIGH * 4 + 5, data);
+	ST7565_drawstring_pixel(52, LINE_HIGH * 6 + 5, data);
+	sprintf(data, "%5d%c", spd[0], '\0');
+	ST7565_drawstring_pixel(96, LINE_HIGH * 4 + 3, data);
+	sprintf(data, "%5d%c", spd[1], '\0');
+	ST7565_drawstring_pixel(96, LINE_HIGH * 5 + 4, data);
+	sprintf(data, "%5d%c", spd[2], '\0');
+	ST7565_drawstring_pixel(96, LINE_HIGH * 6 + 5, data);
 
-	ST7565_drawstring_pixel(6, LINE_HIGH * 5 + 6, "apply pos");
-	ST7565_drawstring_pixel(6, LINE_HIGH * 6 + 7, "apply mov");
 
-	ST7565_drawstring_pixel(100, LINE_HIGH * 4 + 3, "[cm]");
-	ST7565_drawline(0, LINE_HIGH * 2 + 1, 93, LINE_HIGH * 2 + 1, 255);
-	ST7565_drawline(12, LINE_HIGH * 1 + 2, 12, LINE_HIGH * 5 + 4, 255);
-	ST7565_drawline(50, LINE_HIGH * 1 + 2, 50, LINE_HIGH * 5 + 4, 255);
-	ST7565_drawline(94, LINE_HIGH * 1 + 2, 94, LINE_HIGH * 5 + 4, 255);
-	draw_coordinate_diagram(100, 5, 255);
-	arrowLinePos = 1;
-	draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos + 2, 255);
+	//ST7565_drawstring_line(100, 0, "[cm]");
+	ST7565_drawline(0, LINE_HIGH * 4 + 1, 127, LINE_HIGH * 4 + 1, 255);
+	ST7565_drawline(12, LINE_HIGH * 3 + 2, 12, LINE_HIGH * 7 + 4, 255);
+	ST7565_drawline(50, LINE_HIGH * 3 + 2, 50, LINE_HIGH * 7 + 4, 255);
+	ST7565_drawline(94, LINE_HIGH * 3 + 2, 94, LINE_HIGH * 7 + 4, 255);
+	draw_coordinate_diagram(100, 0, 255);
+	arrowLinePos = 3;
+	draw_arrow(0, arrowLinePos * LINE_HIGH + arrowLinePos, 255);
 	markVerticalPos = 0;
 }
+
 
 void draw_st4_settings(){
 	ST7565_drawstring_line(10, 0, "SETTINGS");
@@ -313,6 +385,7 @@ void draw_st4_settings(){
 
 }
 
+
 void draw_arrow(uint8_t x, uint8_t y, uint8_t color){
 	int yy=0;
 	for(int xx=2; xx<3; ++xx)
@@ -331,15 +404,17 @@ void draw_arrow(uint8_t x, uint8_t y, uint8_t color){
 		ST7565_setpixel(x+xx, y+yy, color);
 }
 
-void draw_coordinate_diagram(uint8_t x, uint8_t y, uint8_t color){
-	ST7565_drawline(x, y, x, y+25, 255);
-	ST7565_drawline(x, y+25, x+25, y+25, 255);
-	ST7565_drawline(x, y+25, x+15, y+10, 255);
 
-	ST7565_drawchar_pixel(x+21, y+17, 'x');
-	ST7565_drawchar_pixel(x+17, y+6, 'y');
+void draw_coordinate_diagram(uint8_t x, uint8_t y, uint8_t color){
+	ST7565_drawline(x, y, x, y+15, 255);
+	ST7565_drawline(x, y+15, x+20, y+15, 255);
+	ST7565_drawline(x, y+15, x+13, y+6, 255);
+
+	ST7565_drawchar_pixel(x+22, y+9, 'x');
+	ST7565_drawchar_pixel(x+16, y+2, 'y');
 	ST7565_drawchar_pixel(x+2, y, 'z');
 }
+
 
 void draw_edit_mark(bool on){
 	uint8_t color;
@@ -348,17 +423,18 @@ void draw_edit_mark(bool on){
 	else
 		color=0;
 
-	ST7565_setpixel(125, 59, color);
-	ST7565_setpixel(126, 59, color);
-	ST7565_setpixel(127, 59, color);
-	ST7565_setpixel(125, 60, color);
-	ST7565_setpixel(125, 61, color);
-	ST7565_setpixel(126, 61, color);
-	ST7565_setpixel(125, 62, color);
-	ST7565_setpixel(125, 63, color);
-	ST7565_setpixel(126, 63, color);
-	ST7565_setpixel(127, 63, color);
+	ST7565_setpixel(0, 0, color);
+	ST7565_setpixel(1, 0, color);
+	ST7565_setpixel(2, 0, color);
+	ST7565_setpixel(0, 1, color);
+	ST7565_setpixel(0, 2, color);
+	ST7565_setpixel(1, 2, color);
+	ST7565_setpixel(0, 3, color);
+	ST7565_setpixel(0, 4, color);
+	ST7565_setpixel(1, 4, color);
+	ST7565_setpixel(2, 4, color);
 }
+
 
 double updateDoubleValue(uint8_t xDraw, uint8_t yDraw, uint8_t charWidth, bool sign, char* format, double oldValue){
 	KeyboardButtons buttState = NONE;
@@ -395,6 +471,51 @@ double updateDoubleValue(uint8_t xDraw, uint8_t yDraw, uint8_t charWidth, bool s
 				ST7565_drawstring_pixel(xDraw, yDraw, data);
 				ST7565_display();
 				if(--exit <= 1){
+					draw_edit_mark(false);
+					ST7565_display();
+					return sig * val;
+				}
+			}
+		}
+	}
+}
+
+
+int updateIntegerValue(uint8_t xDraw, uint8_t yDraw, uint8_t charWidth, bool sign, char* format, int oldValue){
+	KeyboardButtons buttState = NONE;
+	int exit = charWidth;
+	int val = 0;
+	int sig = 1;
+	draw_edit_mark(true);
+	ST7565_display();
+	while(1){
+		if(keyboard.stateChanged){
+			buttState = ModKB4x4_getButton(ModKB4x4_readKeyboard(&keyboard));
+
+			if(buttState == BUTT_B){
+				sprintf(data, format, oldValue, '\0');
+				ST7565_drawstring_pixel(xDraw, yDraw, data);
+				ST7565_display();
+				draw_edit_mark(false);
+				ST7565_display();
+				return oldValue;
+			} else if(sign && exit == charWidth){
+				if(buttState >= BUTT_1 && buttState <= BUTT_9)
+					sig = 1;
+				else if(buttState == BUTT_0)
+					sig = -1;
+				else
+					continue;
+				--exit;
+				sprintf(data, format, sig * val, '\0');
+				ST7565_drawstring_pixel(xDraw, yDraw, data);
+				ST7565_display();
+			}else if(buttState >= BUTT_0 && buttState <= BUTT_9){
+				val += buttState * pow(10, exit-1);
+				sprintf(data, format, sig * val, '\0');
+				ST7565_drawstring_pixel(xDraw, yDraw, data);
+				ST7565_display();
+				if(--exit <= 0){
 					draw_edit_mark(false);
 					ST7565_display();
 					return sig * val;
