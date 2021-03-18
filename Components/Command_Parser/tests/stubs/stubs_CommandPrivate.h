6c9433c551ba46c094a5a728eb295aa19e5882a8
Pawel Piskorz <ppiskorz0@gmail.com>
2021-03-18 22:08:49 +0100

    UTs for Command_Parser -#4  -add UTs for Command_Parser

---
+++ Components/Command_Parser/tests/stubs/stubs_CommandPrivate.h
@@ -0,0 +1,15 @@
+/* ############################################################################################ *
+ *                                LICENSE INFORMATIONS                                          *
+ * ============================================================================================
+ * Copyright (c) 2020-2021 Paweł Piskorz
+ * Licensed under the Eclipse Public License 2.0
+ * See attached LICENSE file
+ * ############################################################################################ */
+
+#ifndef STUBS_COMMANDRIVATE_H_
+#define STUBS_COMMANDPRIVATE_H_
+
+Std_Err step_U10(SystemCommand* cmd);
+Std_Err step_U11(SystemCommand* cmd);
+
+#endif /* STUBS_COMMANDPRIVATE_H_ */
